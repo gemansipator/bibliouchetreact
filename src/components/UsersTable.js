788b8e833af917666890e7dc142a2b54dee29cb5
gemansipator <gemansipator@gmail.com>
2025-06-29 12:38:04 +0300

    нули в ячейках

--- src/components/UsersTable.js
+++ src/components/UsersTable.js
@@ -42,50 +42,105 @@ function UsersTable({ theme }) {
                             <th>В т.ч. РДЧ (из гр.3)</th>
                         </tr>
                         <tr className="header">
-                            <th>1</th>
-                            <th>2</th>
-                            <th>3</th>
-                            <th>4</th>
-                            <th>5</th>
-                            <th>6</th>
-                            <th>7</th>
-                            <th>8</th>
-                            <th>9</th>
-                            <th>10</th>
-                            <th>11</th>
-                            <th>12</th>
-                            <th>13</th>
-                            <th>14</th>
-                            <th>15</th>
-                            <th>16</th>
-                            <th>17</th>
-                            <th>18</th>
-                            <th>19</th>
-                            <th>20</th>
-                            <th>21</th>
+                            <th>1</th><th>2</th><th>3</th><th>4</th><th>5</th><th>6</th><th>7</th>
+                            <th>8</th><th>9</th><th>10</th><th>11</th><th>12</th><th>13</th><th>14</th><th>15</th><th>16</th><th>17</th><th>18</th><th>19</th><th>20</th><th>21</th>
                         </tr>
                         </thead>
                         <tbody>
                         <tr>
                             <td className="day-cell">Состоит к началу месяца</td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td></tr>
+                            <td><input type="number" min="0" step="1" defaultValue="0" className="table-input" /></td>
+                            <td><input type="number" min="0" step="1" defaultValue="0" className="table-input" /></td>
+                            <td><input type="number" min="0" step="1" defaultValue="0" className="table-input" /></td>
+                            <td><input type="number" min="0" step="1" defaultValue="0" className="table-input" /></td>
+                            <td><input type="number" min="0" step="1" defaultValue="0" className="table-input" /></td>
+                            <td><input type="number" min="0" step="1" defaultValue="0" className="table-input" /></td>
+                            <td><input type="number" min="0" step="1" defaultValue="0" className="table-input" /></td>
+                            <td><input type="number" min="0" step="1" defaultValue="0" className="table-input" /></td>
+                            <td><input type="number" min="0" step="1" defaultValue="0" className="table-input" /></td>
+                            <td><input type="number" min="0" step="1" defaultValue="0" className="table-input" /></td>
+                            <td><input type="number" min="0" step="1" defaultValue="0" className="table-input" /></td>
+                            <td><input type="number" min="0" step="1" defaultValue="0" className="table-input" /></td>
+                            <td><input type="number" min="0" step="1" defaultValue="0" className="table-input" /></td>
+                            <td><input type="number" min="0" step="1" defaultValue="0" className="table-input" /></td>
+                            <td><input type="number" min="0" step="1" defaultValue="0" className="table-input" /></td>
+                            <td><input type="number" min="0" step="1" defaultValue="0" className="table-input" /></td>
+                            <td><input type="number" min="0" step="1" defaultValue="0" className="table-input" /></td>
+                            <td><input type="number" min="0" step="1" defaultValue="0" className="table-input" /></td>
+                            <td><input type="number" min="0" step="1" defaultValue="0" className="table-input" /></td>
+                            <td><input type="number" min="0" step="1" defaultValue="0" className="table-input" /></td>
+                        </tr>
                         {days.map(day => (
-                            <tr key={day}><td className="day-cell">{day}</td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td></tr>
+                            <tr key={day}>
+                                <td className="day-cell">{day}</td>
+                                <td><input type="number" min="0" step="1" defaultValue="0" className="table-input" /></td>
+                                <td><input type="number" min="0" step="1" defaultValue="0" className="table-input" /></td>
+                                <td><input type="number" min="0" step="1" defaultValue="0" className="table-input" /></td>
+                                <td><input type="number" min="0" step="1" defaultValue="0" className="table-input" /></td>
+                                <td><input type="number" min="0" step="1" defaultValue="0" className="table-input" /></td>
+                                <td><input type="number" min="0" step="1" defaultValue="0" className="table-input" /></td>
+                                <td><input type="number" min="0" step="1" defaultValue="0" className="table-input" /></td>
+                                <td><input type="number" min="0" step="1" defaultValue="0" className="table-input" /></td>
+                                <td><input type="number" min="0" step="1" defaultValue="0" className="table-input" /></td>
+                                <td><input type="number" min="0" step="1" defaultValue="0" className="table-input" /></td>
+                                <td><input type="number" min="0" step="1" defaultValue="0" className="table-input" /></td>
+                                <td><input type="number" min="0" step="1" defaultValue="0" className="table-input" /></td>
+                                <td><input type="number" min="0" step="1" defaultValue="0" className="table-input" /></td>
+                                <td><input type="number" min="0" step="1" defaultValue="0" className="table-input" /></td>
+                                <td><input type="number" min="0" step="1" defaultValue="0" className="table-input" /></td>
+                                <td><input type="number" min="0" step="1" defaultValue="0" className="table-input" /></td>
+                                <td><input type="number" min="0" step="1" defaultValue="0" className="table-input" /></td>
+                                <td><input type="number" min="0" step="1" defaultValue="0" className="table-input" /></td>
+                                <td><input type="number" min="0" step="1" defaultValue="0" className="table-input" /></td>
+                                <td><input type="number" min="0" step="1" defaultValue="0" className="table-input" /></td>
+                            </tr>
                         ))}
-                        <tr><td className="day-cell">Всего за месяц</td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td></tr>
-                        <tr><td className="day-cell">Итого с начала года</td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td></tr>
+                        <tr>
+                            <td className="day-cell">Всего за месяц</td>
+                            <td><input type="number" min="0" step="1" defaultValue="0" className="table-input" /></td>
+                            <td><input type="number" min="0" step="1" defaultValue="0" className="table-input" /></td>
+                            <td><input type="number" min="0" step="1" defaultValue="0" className="table-input" /></td>
+                            <td><input type="number" min="0" step="1" defaultValue="0" className="table-input" /></td>
+                            <td><input type="number" min="0" step="1" defaultValue="0" className="table-input" /></td>
+                            <td><input type="number" min="0" step="1" defaultValue="0" className="table-input" /></td>
+                            <td><input type="number" min="0" step="1" defaultValue="0" className="table-input" /></td>
+                            <td><input type="number" min="0" step="1" defaultValue="0" className="table-input" /></td>
+                            <td><input type="number" min="0" step="1" defaultValue="0" className="table-input" /></td>
+                            <td><input type="number" min="0" step="1" defaultValue="0" className="table-input" /></td>
+                            <td><input type="number" min="0" step="1" defaultValue="0" className="table-input" /></td>
+                            <td><input type="number" min="0" step="1" defaultValue="0" className="table-input" /></td>
+                            <td><input type="number" min="0" step="1" defaultValue="0" className="table-input" /></td>
+                            <td><input type="number" min="0" step="1" defaultValue="0" className="table-input" /></td>
+                            <td><input type="number" min="0" step="1" defaultValue="0" className="table-input" /></td>
+                            <td><input type="number" min="0" step="1" defaultValue="0" className="table-input" /></td>
+                            <td><input type="number" min="0" step="1" defaultValue="0" className="table-input" /></td>
+                            <td><input type="number" min="0" step="1" defaultValue="0" className="table-input" /></td>
+                            <td><input type="number" min="0" step="1" defaultValue="0" className="table-input" /></td>
+                            <td><input type="number" min="0" step="1" defaultValue="0" className="table-input" /></td>
+                        </tr>
+                        <tr>
+                            <td className="day-cell">Итого с начала года</td>
+                            <td><input type="number" min="0" step="1" defaultValue="0" className="table-input" /></td>
+                            <td><input type="number" min="0" step="1" defaultValue="0" className="table-input" /></td>
+                            <td><input type="number" min="0" step="1" defaultValue="0" className="table-input" /></td>
+                            <td><input type="number" min="0" step="1" defaultValue="0" className="table-input" /></td>
+                            <td><input type="number" min="0" step="1" defaultValue="0" className="table-input" /></td>
+                            <td><input type="number" min="0" step="1" defaultValue="0" className="table-input" /></td>
+                            <td><input type="number" min="0" step="1" defaultValue="0" className="table-input" /></td>
+                            <td><input type="number" min="0" step="1" defaultValue="0" className="table-input" /></td>
+                            <td><input type="number" min="0" step="1" defaultValue="0" className="table-input" /></td>
+                            <td><input type="number" min="0" step="1" defaultValue="0" className="table-input" /></td>
+                            <td><input type="number" min="0" step="1" defaultValue="0" className="table-input" /></td>
+                            <td><input type="number" min="0" step="1" defaultValue="0" className="table-input" /></td>
+                            <td><input type="number" min="0" step="1" defaultValue="0" className="table-input" /></td>
+                            <td><input type="number" min="0" step="1" defaultValue="0" className="table-input" /></td>
+                            <td><input type="number" min="0" step="1" defaultValue="0" className="table-input" /></td>
+                            <td><input type="number" min="0" step="1" defaultValue="0" className="table-input" /></td>
+                            <td><input type="number" min="0" step="1" defaultValue="0" className="table-input" /></td>
+                            <td><input type="number" min="0" step="1" defaultValue="0" className="table-input" /></td>
+                            <td><input type="number" min="0" step="1" defaultValue="0" className="table-input" /></td>
+                            <td><input type="number" min="0" step="1" defaultValue="0" className="table-input" /></td>
+                        </tr>
                         </tbody>
                     </table>
                 </div>
